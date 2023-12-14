25b251deabfb550bcaeced4f4831767b4207204d
Matt Sivertson <sivertm@adskc02gl4edq05n.lan>
2023-12-14 11:14:44 -0800

    interim state before doing split solution

--- 2023/10/main.c
+++ 2023/10/main.c
@@ -7,17 +7,6 @@
 #include <limits.h>
 #include <pthread.h>
 
-typedef struct {
-	int x;
-	int y;
-} move_t;
-
-/*move_t *getMovement(int x, int y, char ch) {
-	switch(ch) {
-		case '|': return 
-	}
-}*/
-
 int resolveLine(int lineNums[], int numEntries, bool prependMode) {
 	int numDiffs = numEntries - 1;
 	int diffs[numDiffs];
@@ -47,6 +36,10 @@ int resolveLine(int lineNums[], int numEntries, bool prependMode) {
 	}
 }
 
+bool isPipe(char ch) {
+	return ch == '-' || ch == '7' || ch == 'J' || ch == '|' || ch == 'F' || ch == 'L';
+}
+
 bool isValidRight(char ch) {
 	return ch == '-' || ch == '7' || ch == 'J';
 }
@@ -61,6 +54,14 @@ bool isValidUp(char ch) {
 
 bool isValidDown(char ch) {
 	return ch == '|' || ch == 'J' || ch == 'L';
+}
+
+bool isValidVertical(char ch) {
+	return isValidUp(ch) || isValidDown(ch);
+}
+
+bool isValidHorizontal(char ch) {
+	return isValidLeft(ch) || isValidRight(ch);
 }
 
 char figurePipe(char **map, int currentX, int currentY) {
@@ -86,6 +87,17 @@ char figurePipe(char **map, int currentX, int currentY) {
 	printf("Either a bug or an invalid map\n");
 	return '.';
 }
+
+
+/*     Doors
+ *   JL can go up
+ *   7F can go down
+ *   L can go left
+ *   F
+ *
+ *   J can go right
+ *   7
+ */
 
 
 //Need to fix the up/downs here
@@ -117,24 +129,26 @@ bool shouldMoveLeft(char ch, int currentX, int currentY, int lastX, int lastY) {
 	return false;
 }
 
-int exploreTubes(char **map, int currentX, int currentY, int lastX, int lastY, int distanceTraveled) {
+int exploreTubes(char **map, char **mapStencil, int currentX, int currentY) {
+	int lastX = currentX;
+	int lastY = currentY;
+	int distanceTraveled = 0;
 
 	char ch = map[currentY][currentX];
 	if (ch == 'S') {
 		ch = figurePipe(map, currentX, currentY);
+		printf("S cell is a %c\n", ch);
 	}
 
 	
-	for (;;) {
-	printf("exploreTubes current = (%d, %d), last = (%d, %d)\n", currentX, currentY, lastX, lastY);
+	while (ch != 'S') {
+		//printf("exploreTubes current = (%d, %d), last = (%d, %d)\n", currentX, currentY, lastX, lastY);
+		mapStencil[currentY][currentX] = ch;	
 		distanceTraveled++;
-		//if (distanceTraveled > 2) {
-		//	return 0;
-		//}
 
 		int deltaX = 0;
 		int deltaY = 0;
-		printf("tube = %c valids = (%d, %d, %d, %d) shoulds = %d, %d, %d, %d\n",
+		/*printf("tube = %c valids = (%d, %d, %d, %d) shoulds = %d, %d, %d, %d\n",
 			ch, 
 			isValidDown(ch),
 			isValidUp(ch),
@@ -144,24 +158,21 @@ int exploreTubes(char **map, int currentX, int currentY, int lastX, int lastY, i
 			shouldMoveDown(ch, currentX, currentY, lastX, lastY),
 			shouldMoveLeft(ch, currentX, currentY, lastX, lastY),
 			shouldMoveRight(ch, currentX, currentY, lastX, lastY));
+			*/
 
 
 		if (shouldMoveUp(ch, currentX, currentY, lastX, lastY)) {
 			deltaY = -1;
-			printf("Moving up\n");
-			//return exploreTubes(map, currentX, currentY - 1, currentX, currentY, distanceTraveled + 1);
+			//printf("Moving up\n");
 		} else if (shouldMoveDown(ch, currentX, currentY, lastX, lastY)) {
 			deltaY = 1;
-			printf("Moving down\n");
-			//return exploreTubes(map, currentX, currentY + 1, currentX, currentY, distanceTraveled + 1);
+			//printf("Moving down\n");
 		} else if (shouldMoveLeft(ch, currentX, currentY, lastX, lastY)) {
 			deltaX = -1;
-			printf("Moving left\n");
-			//return exploreTubes(map, currentX - 1, currentY, currentX, currentY, distanceTraveled + 1);
+			//printf("Moving left\n");
 		} else if (shouldMoveRight(ch, currentX, currentY, lastX, lastY)) {
 			deltaX = 1;
-			printf("Moving right\n");
-			//return exploreTubes(map, currentX + 1, currentY, currentX, currentY, distanceTraveled + 1);
+			//printf("Moving right\n");
 		}
 
 		lastX = currentX;
@@ -171,18 +182,132 @@ int exploreTubes(char **map, int currentX, int currentY, int lastX, int lastY, i
 		currentY += deltaY;
 
 		ch = map[currentY][currentX];
-		if (ch == 'S') {
-			return distanceTraveled;
-		} 
 	}
 
-	return 0;
+	return distanceTraveled;
 }
 
 
 
+bool canMoveUp(char **mapStencil, int mapWidth, int mapHeight, int x, int y) {
+	if (y > 0) {
+		char ch = mapStencil[y-1][x];
+		char nextCh = mapStencil[y-1][x+1];
+		if (ch == '.') {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool canMoveDown(char **mapStencil, int mapWidth, int mapHeight, int x, int y) {
+	if (y < mapHeight-1) {
+		char ch = mapStencil[y+1][x];
+		if (ch == '.') {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool canMoveLeft(char **mapStencil, int mapWidth, int mapHeight, int x, int y) {
+	if (x > 0) {
+		char ch = mapStencil[y][x-1];
+		if (ch == '.') {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool canMoveRight(char **mapStencil, int mapWidth, int mapHeight, int x, int y) {
+	if (x < mapWidth-1) {
+		char ch = mapStencil[y][x+1];
+		if (ch == '.') {
+			return true;
+		}
+	}
+	return false;
+}
+
+void floodFill(char **map, char **mapStencil, int mapWidth, int mapHeight, int x, int y) {
+	if (mapStencil[y][x] == '.') {
+		mapStencil[y][x] = '0';
+	} else {
+	//	mapStencil[y][x] = 'P';
+	}
+	if (canMoveLeft(mapStencil, mapWidth, mapHeight, x, y)) {
+		floodFill(map, mapStencil, mapWidth, mapHeight, x-1, y);
+	}
+	if (canMoveRight(mapStencil, mapWidth, mapHeight, x, y)) {
+		floodFill(map, mapStencil, mapWidth, mapHeight, x+1, y);
+	}
+	if (canMoveUp(mapStencil, mapWidth, mapHeight, x, y)) {
+		if (mapStencil[y-1][x] == 'J' && mapStencil[y-1][x+1] == 'L') {
+			printf("FOUND A DOOR!!!!\n");	
+			y--;
+			char left = mapStencil[y][x];
+			char right = mapStencil[y][x+1];
+			do {
+				y--;
+				left = mapStencil[y][x];
+				right = mapStencil[y][x+1];
+			} while (isValidVertical(left) || isValidVertical(right));
+
+			if (!isPipe(left)) {
+				floodFill(map, mapStencil, mapWidth, mapHeight, x, y);
+			} else {
+				floodFill(map, mapStencil, mapWidth, mapHeight, x+1, y);
+			}
+		}
+		floodFill(map, mapStencil, mapWidth, mapHeight, x, y-1);
+	}
+	if (canMoveDown(mapStencil, mapWidth, mapHeight, x, y)) {
+		floodFill(map, mapStencil, mapWidth, mapHeight, x, y+1);
+	}
+}
+
+void findEnclosures(char **map, char **mapStencil, int mapWidth, int mapHeight) {
+	//Go around the edges and floodfill
+	for (int y = 0; y < mapHeight; y++ )
+	{
+		if (y == 0 || y == mapHeight-1) {
+			for (int x = 0; x < mapWidth-1; x++) {
+				if (mapStencil[y][x] == '.') {
+					floodFill(map, mapStencil, mapWidth, mapHeight, x, y);
+				}
+			}
+			
+		} else {
+			if (mapStencil[y][0] == '.') {
+				floodFill(map, mapStencil, mapWidth, mapHeight, 0, y);
+			}
+			if (mapStencil[y][mapWidth] == '.') {
+				floodFill(map, mapStencil, mapWidth, mapHeight, mapWidth, y);
+			}
+		}
+	}
+}
+
+int countDots(char **stencilMap, int maxWidth, int maxHeight) {
+	int numDots = 0;
+	for (int y = 0; y < maxHeight; y++) {
+		for (int x = 0; x < maxWidth; x++) {
+			if (stencilMap[y][x] == '.') {
+				numDots++;
+			}
+		}
+	}
+	return numDots;
+}
+
 int main(int argc, char *argv[]) {
-	FILE *fp = fopen("input.txt", "r");
+	printf("argc = %d\n", argc);
+	char *inputFile = "input.txt";
+	if (argc == 2) {
+		inputFile = argv[1];
+	}	
+	FILE *fp = fopen(inputFile, "r");
 	if (fp == NULL) {
 		fprintf(stderr, "File input.txt not found\n");
 		return -1;
@@ -211,16 +336,34 @@ int main(int argc, char *argv[]) {
 		numRows++;
 	} 
 
-	printf("Start pipe must be %c\n", figurePipe(map, startX, startY));
+	int rowWidth = strlen(map[0]) + 1;
+	char **mapStencil = (char **)malloc(sizeof(char *) * numRows);
+	for (int i = 0; i < numRows; i++) {
+		mapStencil[i] = (char *)malloc(rowWidth);
+		memset(mapStencil[i], '.', rowWidth);
+		mapStencil[rowWidth] = 0;
+	}
 
-	printf("StartLoc = (%d, %d)\n", startX, startY);
-	long totalDistance = exploreTubes(map, startX, startY, startX, startY, 0);
+	long totalDistance = exploreTubes(map, mapStencil, startX, startY);
 
 	printf("Total distance = %ld, part 1 answer = %ld\n", totalDistance, totalDistance / 2);
 
 	for (int i = 0; i < numRows; i++) {
-		//printf("%s\n", map[i]);
+		printf("%s\n", mapStencil[i]);
+	}
+
+	findEnclosures(map, mapStencil, rowWidth, numRows);
+
+	printf("\n\n");
+	for (int i = 0; i < numRows; i++) {
+		printf("%s\n", mapStencil[i]);
+	}
+
+	printf("numDots = %d\n", countDots(mapStencil, rowWidth, numRows));
+
+	for (int i = 0; i < numRows; i++) {
 		free(map[i]);
+		free(mapStencil[i]);
 	}
 	free(map);
 
